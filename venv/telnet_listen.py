2273f02e7af9db40d01a1c627e7ca3f90563841f
Korkibucek <robert@morgan.vg>
2018-02-26 12:31:53 +0100

    Adding telnet listener

--- venv/telnet_listen.py
+++ venv/telnet_listen.py
@@ -9,9 +9,6 @@ tn = telnetlib.Telnet("dxc.9a5k.com", "7373")
 tn.read_until(b"Please enter your call:", 2)
 tn.write(b"\n")
 
-tn.read_until(b"=>", 2)
-tn.write(b"exit\n")
-
 print(tn.read_all().decode('ascii'))
 
 tn.close
