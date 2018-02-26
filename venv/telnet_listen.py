2b3a895a49464216f07fd41b8b36a1ff2ede9714
Korkibucek <robert@morgan.vg>
2018-02-26 12:28:24 +0100

    Adding telnet listener

--- venv/telnet_listen.py
+++ venv/telnet_listen.py
@@ -4,8 +4,7 @@ import socket
 import sys
 import telnetlib
 
-tn = telnetlib.Telnet("dxc.9a5k.com")
-tn = telnetlib.TELNET_PORT("7373")
+tn = telnetlib.Telnet("dxc.9a5k.com", "7373")
 
 tn.read_until(b"Please enter your call:", 2)
 tn.write(b"\n")
